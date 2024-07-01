882f5c3ddaa2dab00838d28c1d70d0b45006f209
shadowrunner11 <pieroramirez810@gmail.com>
2024-07-01 10:20:00 -0500

    chore: added storybook

--- server/index.cjs
+++ server/index.cjs
@@ -1,10 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express');
 const { join } = require('path');
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-const publicPath = join(__dirname, 'public');
+const isLocalDev = !!process.env.IS_LOCAL_DEV
+
+const publicPath = join(__dirname, isLocalDev? '..' : '', 'storybook-static',);
 
 app
   .use(express.static(publicPath))
